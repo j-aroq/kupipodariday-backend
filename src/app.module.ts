744b3d172af599abd6d409ee1952b45252eff2c2
Julia Zhidkova <zhidkova.julia@gmail.com>
2023-07-18 13:16:59 +0200

    Realized database schemes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { WishesModule } from './wishes/wishes.module';
+import { UsersModule } from './users/users.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'student',
+      password: 'student',
+      database: 'nest_project',
+      entities: [],
+      synchronize: true,
+    }),
+    WishesModule,
+    UsersModule,
+    WishlistsModule,
+    OffersModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
